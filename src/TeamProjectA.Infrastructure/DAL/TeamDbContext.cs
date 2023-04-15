662d8b9af130d060b676bf8ff370861218f811c2
Namlesz <nejmlesz@gmail.com>
2023-04-15 13:11:53 +0200

    .

--- src/TeamProjectA.Infrastructure/DAL/TeamDbContext.cs
+++ src/TeamProjectA.Infrastructure/DAL/TeamDbContext.cs
@@ -5,16 +5,16 @@ using TeamProjectA.Infrastructure.Settings;
 
 namespace TeamProjectA.Infrastructure.DAL;
 
-public class WorkoutsContext : IWorkoutsContext
+public class TeamDbContext : ITeamDbContext
 {
     private readonly IMongoDatabase _db;
     private readonly string _collectionName;
 
-    public WorkoutsContext(IMongoDatabase db, IOptions<DbConfig> dbConfig)
+    public TeamDbContext(IMongoDatabase db, IOptions<DbConfig> dbConfig)
     {
         _db = db;
         _collectionName = dbConfig.Value.WorkoutsCollection;
     }
 
-    public IMongoCollection<WorkoutEntity> GetCollection() => _db.GetCollection<WorkoutEntity>(_collectionName);
+    public IMongoCollection<WorkoutEntity> WorkoutsCollection => _db.GetCollection<WorkoutEntity>(_collectionName);
 }
