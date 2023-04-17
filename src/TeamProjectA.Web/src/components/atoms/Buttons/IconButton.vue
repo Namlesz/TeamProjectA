05fa1a70b1ba4b34f20dde8dc7dcd033ee684cf1
Adam Sporczyk <adamspo00@gmail.com>
2023-04-17 19:15:56 +0200

    Update IconButton.vue

--- src/TeamProjectA.Web/src/components/atoms/Buttons/IconButton.vue
+++ src/TeamProjectA.Web/src/components/atoms/Buttons/IconButton.vue
@@ -4,8 +4,9 @@ defineProps<{ icon?: string, variant?: 'default' | 'noBackground' }>()
 
 </script>
 <template>
-  <v-btn :icon='icon'
-         variant='plain'
-         :ripple='false'
+  <v-btn
+    :icon='icon'
+    variant='plain'
+    :ripple='false'
   />
 </template>
